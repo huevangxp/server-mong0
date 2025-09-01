d81a47ce28bd65e5b5db8e4cf296b52ff576a64e
huevangxp <huevang933@gmail.com>
2025-09-01 15:14:01 +0700

    styart

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// MongoDB connection
+// mongoose.connect('mongodb://localhost:27017/yourdb', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+// .then(() => console.log('Connected to MongoDB'))
+// .catch(err => console.error('MongoDB connection error:', err));
+
+// Basic route
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+// Start server
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
