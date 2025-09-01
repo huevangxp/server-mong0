ad35d6846cc6844cedf7a8bf7c5f333ea50baff0
huevangxp <huevang933@gmail.com>
2025-09-01 15:16:23 +0700

    commit Mon 09/01/2025 15:16:23.15

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//     useNewUrlParser: true,
 //     useUnifiedTopology: true
 // })
 // .then(() => console.log('Connected to MongoDB'))
