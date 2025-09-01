dec817f76161ad27bf8a8d09f0ed06436c3d677c
huevangxp <huevang933@gmail.com>
2025-09-01 15:18:15 +0700

    commit Mon 09/01/2025 15:18:15.12

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 
+import 'dotenv/config';
+// import mongoose from 'mongoose'; --- IGNORE ---  
+import routes from './src/routes/routes.js';
+
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.send('Server is running');
-});
+ 
+app.use('/api', routes);
 
 // Start server
 const PORT = 5000;
